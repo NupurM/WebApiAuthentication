a1d05a18f0dd0ed3f13260df1a3deece0338baa6
NM <NM>
2018-08-27 16:05:28 -0400

    Implementing Basic Authentication

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        [BasicAuthentication]
+        public HttpResponseMessage Get()
+        {
+            string username = Thread.CurrentPrincipal.Identity.Name;
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                switch (username.ToLower())
+                {
+                    case "male":
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e => e.Gender == "male").ToList());
+                    case "female":
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e => e.Gender == "female").ToList());
+                    default:
+                        return Request.CreateResponse(HttpStatusCode.BadRequest);
+                }
+            }
+        }
+    }
+}
